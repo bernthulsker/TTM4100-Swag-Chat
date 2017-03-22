0fd8e94b3734f8867c95fb1cfbe42af2efdb554a
bernthulsker <jon_eirik_@hotmail.com>
2017-03-22 11:44:44 +0100

    why?

--- Client/Client.py
+++ Client/Client.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import socket
+import json
 from MessageReceiver import MessageReceiver
 from MessageParser import MessageParser
 
@@ -21,15 +22,17 @@ class Client:
     def run(self):
         # Initiate the connection to the server
         self.connection.connect((self.host, self.server_port))
+        self.thread = MessageReceiver(self, self.connection)
 
         while (1):
             new_payload = self.take_input()
+            print new_payload
 
-            if payload['request'] == 'disconnect':
+            if new_payload['request'] == 'disconnect':
                 self.disconnect()
                 exit()
 
-            send_payload(json.dumps(new_payload))
+            self.send_payload(json.dumps(new_payload))
 
         
     def disconnect(self):
@@ -45,10 +48,10 @@ class Client:
         
     def take_input(self):
         payload = {}
-        payload['request'] = input('Enter request:')
+        payload['request'] = raw_input('Enter request:')
 
-        if payload['request'] == login or payload['request']==message:
-            payload['content'] = input('Enter content:')
+        if payload['request'] == "login" or payload['request']=="message":
+            payload['content'] = raw_input('Enter content:')
 
         return payload
     # More methods may be needed!
